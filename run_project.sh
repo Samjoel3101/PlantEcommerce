ce247cb40b05fc6a1375f27b803e757c135454dc
Samjoel3101 <samjoel3101@gmail.com>
2021-01-02 21:44:50 +0530

    added setup shell scripts

---
+++ run_project.sh
@@ -0,0 +1,4 @@
+cd Django_React_Project
+python manage.py makemigrations 
+python manage.py migrate
+python manage.py runserver localhost:8000
---
+++ setup_project.sh
@@ -0,0 +1,9 @@
+pipenv install 
+cd Django_React_Project/frontend
+npm install
+
+cd ..
+cd .. 
+pipenv shell
+echo $PWD
+
